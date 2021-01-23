fe9c6544cf3dd1f6d42b29efdbd763533ffbe14a
Mnkith <munkithabid@gmail.com>
2021-01-22 20:51:07 -0600

    commit tree 6a6a7ab0f21302c3cd292d9be4f961f9771bc5b1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,47 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/recipes' do
+    @all = Recipe.all
+    erb :index
+  end
 
+  get '/recipes/new' do
+    erb :new
+  end
+
+  
+  get '/recipes/:id/edit' do 
+    @r = Recipe.find(params[:id])
+    erb :edit
+  end
+  
+  patch '/recipes/:id' do 
+    @r = Recipe.find(params[:id])
+    @r.name = params[:name]
+    @r.ingredients = params[:ingredients]
+    @r.cook_time = params[:cook_time]
+    @r.save
+    # binding.pry
+    redirect "/recipes/#{@r.id}"
+  end
+  
+
+  post '/recipes' do
+    @r = Recipe.create(params)
+    # binding.pry
+    redirect "/recipes/#{@r.id}"
+  end
+  
+  get '/recipes/:id' do
+    @r = Recipe.find(params[:id])
+    erb :show
+  end
+
+  delete '/recipes/:id' do #delete action
+    @r = Recipe.find_by_id(params[:id])
+    @r.delete
+    redirect to '/articles'
+  end 
+  
 end
